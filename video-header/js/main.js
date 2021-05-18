d0150970e347de80f1d768a53dc103b973c93b5c
Rasmus Cederdorff <rasmus@cederdorff.com>
2021-05-18 13:52:37 +0200

    video header project added

---
+++ video-header/js/main.js
@@ -0,0 +1,15 @@
+"use strict"; // to enable strict mode and modern JavaScript functionality
+
+// declaring a variable with a value
+let message = "Hi Students";
+
+// accessing the variable and logging it to the console
+console.log(message);
+
+// appending the value from the variable to thee DOM element #content
+document.querySelector("#content").innerHTML = message;
+
+// showing and alert with a message
+function showAlert() {
+  alert('Open your Developer Console!');
+}
