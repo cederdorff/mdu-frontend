9e016d6ead8a8a076a3bf50671af5ce1c6b501e6
Rasmus Cederdorff <rasmus@cederdorff.com>
2021-05-18 14:23:12 +0200

    Update main.js

--- video-header/js/main.js
+++ video-header/js/main.js
@@ -1,15 +1,16 @@
 "use strict"; // to enable strict mode and modern JavaScript functionality
 
+// global variables: video, playPauseBtn
 let video = document.getElementById("header-video");
-let btn = document.querySelector("#play-pause-btn");
+let playPauseBtn = document.querySelector("#play-pause-btn");
 
 // Pause and play the video, and change the button text
 function playPauseVideo() {
   if (video.paused) {
     video.play();
-    btn.innerHTML = "Pause";
+    playPauseBtn.innerHTML = "Pause";
   } else {
     video.pause();
-    btn.innerHTML = "Play";
+    playPauseBtn.innerHTML = "Play";
   }
 }
