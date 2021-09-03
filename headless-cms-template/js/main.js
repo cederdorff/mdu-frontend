9d0d5c3c011de6fdbed4ad99026bfa5a939c252b
Rasmus Cederdorff <rasmus@cederdorff.com>
2021-09-03 13:19:08 +0200

    Update main.js

--- headless-cms-template/js/main.js
+++ headless-cms-template/js/main.js
@@ -1,36 +1,20 @@
 "use strict";
 
-async function getData() {
-  let response = await fetch("http://persons-api.racedev.dk/wp-json/wp/v2/posts");
-  let data = response.json();
-  console.log(data);
-  appendPosts(data);
+async function fetchData() {
+  const url = "https://headlesscms.cederdorff.com/wp-json/wp/v2/posts?_embed";
 }
 
-getData();
 
-// append wp posts to the DOM
-function appendPosts(posts) {
-  let template = "";
-  for (const post of posts) {
-    console.log(post);
-    template += /*html*/`
-      <article>
-        <img src="${post.acf.image.url}">
-        <h2>${post.title.rendered}</h2>
-        <a href="mailto:${post.acf.mail}">${post.acf.mail}</a>
-        <p>${post.acf.age} years old</p>
-      </article>
-    `;
-  }
-  document.getElementById("content").innerHTML = template;
+function appendData(posts) {
+
 }
 
-// get the featured image url
-function getFeaturedImageUrl(post) {
-  let imageUrl = "";
-  if (post._embedded['wp:featuredmedia']) {
-    imageUrl = post._embedded['wp:featuredmedia'][0].source_url;
+function getImageUrl(post) {
+  let url = "";
+  if (post._embedded["wp:featuredmedia"]) {
+    url = post._embedded["wp:featuredmedia"][0].source_url;
+  } else {
+    url = "img/favicon.png";
   }
-  return imageUrl;
+  return url;
 }
