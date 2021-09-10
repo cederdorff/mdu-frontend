525e128b60d173bf6cf646a199754789d07bca3e
Rasmus Cederdorff <rasmus@cederdorff.com>
2021-09-10 09:12:44 +0200

    Update main.js

--- spa-headless-cms-movies-class-structure/scripts/main.js
+++ spa-headless-cms-movies-class-structure/scripts/main.js
@@ -1,11 +1,12 @@
 import Spa from "./spa.js";
 import MovieService from "./movie-service.js";
 
+//init services 
 let spa = new Spa("movies");
 let movieService = new MovieService();
-
 movieService.init();
 
+// maiking event available for global scope
 window.categorySelected = function (id) {
   movieService.getMoviesByCategory(id);
 }
