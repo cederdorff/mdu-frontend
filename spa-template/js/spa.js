007f3d6f7328c3ea94bd074286a1a2bd58dd7c38
Rasmus Cederdorff <rasmus@cederdorff.com>
2021-09-20 17:07:49 +0200

    Update spa.js

--- spa-template/js/spa.js
+++ spa-template/js/spa.js
@@ -1,45 +1,81 @@
-"use strict";
+/**
+ * All routes of the SPA
+ * "path": "id of page in DOM"
+ */
+const _routes = {
+  "#/": "home",
+  "#/about": "about",
+  "#/clients": "clients"
+};
+const _pages = document.querySelectorAll(".page");
+const _basePath = location.pathname.replace("index.html", ""); // remove index.html from path
+const _navLinks = document.querySelectorAll("nav a");
 
-// hide all pages
+/**
+ * Changing display to none for all pages
+ */
 function hideAllPages() {
-  let pages = document.querySelectorAll(".page");
-  for (let page of pages) {
+  for (const page of _pages) {
     page.style.display = "none";
   }
 }
 
-// show page or tab
-function showPage(pageId) {
-  hideAllPages();
-  document.querySelector(`#${pageId}`).style.display = "block";
-  setActiveTab(pageId);
+/**
+ * Navigating SPA to specific page by given path
+ */
+function navigateTo(path) {
+  window.history.pushState({}, path, _basePath + path);
+  showPage(path);
 }
 
-// sets active tabbar/ menu item
-function setActiveTab(pageId) {
-  let pages = document.querySelectorAll(".tabbar a");
-  for (let page of pages) {
-    if (`#${pageId}` === page.getAttribute("href")) {
-      page.classList.add("active");
+/**
+ * Displaying page by given path
+ */
+function showPage(path) {
+  hideAllPages(); // hide all pages
+  document.querySelector(`#${_routes[path]}`).style.display = "block"; // show page by given path
+  setActiveTab(path);
+}
+
+/**
+ * sets active menu item by given path
+ */
+function setActiveTab(path) {
+  for (const link of _navLinks) {
+    if (path === link.getAttribute("href")) {
+      link.classList.add("active");
     } else {
-      page.classList.remove("active");
+      link.classList.remove("active");
     }
   }
 }
 
-// navigate to a new view/page by changing href
-function navigateTo(pageId) {
-  location.href = `#${pageId}`;
-}
-
-// set default page or given page by the hash url
-// function is called 'onhashchange'
-function pageChange() {
-  let page = "home";
-  if (location.hash) {
-    page = location.hash.slice(1);
+/**
+ * Attaching event to nav links and preventing default anchor link event
+ */
+function attachNavLinkEvents() {
+  const navLinks = document.querySelectorAll(".nav-link");
+  for (const link of navLinks) {
+    link.addEventListener("click", function (event) {
+      const path = link.getAttribute("href");
+      navigateTo(path);
+      event.preventDefault();
+    });
   }
-  showPage(page);
 }
 
-pageChange(); // called by default when the app is loaded for the first time
+/**
+ * Initialising the router, calling attachNavLinkEvents(), popstate event and navigateTo()
+ */
+function initRouter() {
+  attachNavLinkEvents();
+  window.addEventListener("popstate", () => showPage(location.hash)); // change page when using back and forth in browser
+
+  let path = "#/"; // default path
+  if (_routes[location.hash]) {
+    path = location.hash;
+  }
+  navigateTo(path);
+}
+
+initRouter();
