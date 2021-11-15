6a303f4227d7352819c958893f4f1d3e0ba801c5
Rasmus Cederdorff <rasmus@cederdorff.com>
2021-11-15 20:32:43 +0100

    Create main.js

---
+++ array-map/js/main.js
@@ -0,0 +1,29 @@
+const persons = [
+	{
+		firstName: "Jane",
+		lastName: "Doe",
+		birthDate: "1992-03-04"
+	},
+	{
+		firstName: "Jens",
+		lastName: "Jensen",
+		birthDate: "1992-07-04"
+	},
+	{
+		firstName: "Birgitte",
+		lastName: "Iversen",
+		birthDate: "1990-10-04"
+	},
+	{
+		firstName: "Lykke",
+		lastName: "Dahlen",
+		birthDate: "1987-06-04"
+	},
+	{
+		firstName: "Kasper",
+		lastName: "Topp",
+		birthDate: "1989-03-07"
+	}
+];
+
+persons.map(person => console.log(person));
