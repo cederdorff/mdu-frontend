54186f233dd9e96253c6d80f91ac5aaf2f10b44b
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-01-27 09:44:57 +0100

    spa templates updated with router

--- spa-products-template/js/app.js
+++ spa-products-template/js/app.js
@@ -1,4 +1,4 @@
-"use strict";
+console.log("app.js ready 🎉");
 
 // =========== Product functionality =========== //
 
@@ -6,26 +6,29 @@
  * Array of product objects
  */
 let products = [
-  // to do
+    // to do
 ];
 
 /**
  * Appending products to the DOM using a for-of loop
  */
 function appendProducts() {
-  // to do
+    // to do
 }
 
 /**
  * Adding a new product to the Array of products
  */
-function addNewProduct() {
-  // to do
+function addNewProduct(event) {
+    console.log(event.target);
+    console.log(event.target.brand.value);
+    event.preventDefault();
+    // to do
 }
 
 /**
  * Searching for products matching the input value
  */
 function search(value) {
-  // to do
+    // to do
 }
