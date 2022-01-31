a78125bd815a865ad31c8bf5a0949f4709db3bea
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-01-31 20:25:42 +0100

    Update main.js

--- fetch-family-members-template/js/main.js
+++ fetch-family-members-template/js/main.js
@@ -6,19 +6,19 @@ let _familyMembers = [];
 /*
 Fetches json data from the file persons.json
 */
-fetch('json/persons.json')
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    _familyMembers = data;
-    // todo: call appendPersons(...)
-  });
+fetch("json/persons.json")
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        _familyMembers = data;
+        // todo: call appendPersons(...)
+    });
 
 /*
 Appends json data to the DOM
 */
 function appendPersons(persons) {
-  // todo: append all persons to the DOM using a for-of loop
+    // todo: append all persons to the DOM using a for-of loop
 }
