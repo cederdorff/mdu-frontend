62f15816833b1ff934c4de559ba98d39cc806fc5
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-01-31 20:25:44 +0100

    Update main.js

--- fetch-family-members/js/main.js
+++ fetch-family-members/js/main.js
@@ -8,10 +8,10 @@ Fetches json data from the file persons.json
 */
 
 async function fetchPersons() {
-  let response = await fetch('json/persons.json');
-  let data = await response.json();
-  _familyMembers = data;
-  appendPersons(_familyMembers);
+    let response = await fetch("json/persons.json");
+    let data = await response.json();
+    _familyMembers = data;
+    appendPersons(_familyMembers);
 }
 
 fetchPersons();
@@ -20,9 +20,9 @@ fetchPersons();
 Appends json data to the DOM
 */
 function appendPersons(persons) {
-  let htmlTemplate = "";
-  for (let person of persons) {
-    htmlTemplate += /*html*/ `
+    let htmlTemplate = "";
+    for (let person of persons) {
+        htmlTemplate += /*html*/ `
       <article>
         <img src="${person.img}">
         <h4>${person.name}</h4>
@@ -31,47 +31,47 @@ function appendPersons(persons) {
         <p>Relation: ${person.relation}</p>
       </article>
     `;
-  }
-  document.querySelector("#persons").innerHTML = htmlTemplate;
+    }
+    document.querySelector("#persons").innerHTML = htmlTemplate;
 }
 
 /**
  * Filtering family members by given searchValue
  */
 function search(searchValue) {
-  searchValue = searchValue.toLowerCase();
-  console.log(searchValue);
+    searchValue = searchValue.toLowerCase();
+    console.log(searchValue);
 
-  let filteredFamilyMembers = [];
+    let filteredFamilyMembers = [];
 
-  for (let familyMember of _familyMembers) {
-    let name = familyMember.name.toLowerCase();
-    if (name.includes(searchValue.toLowerCase())) {
-      filteredFamilyMembers.push(familyMember);
+    for (let familyMember of _familyMembers) {
+        let name = familyMember.name.toLowerCase();
+        if (name.includes(searchValue.toLowerCase())) {
+            filteredFamilyMembers.push(familyMember);
+        }
     }
-  }
-  appendPersons(filteredFamilyMembers);
+    appendPersons(filteredFamilyMembers);
 }
 
 /**
  * Adding a new family member to the _familyMember array
  */
 function add() {
-  let inputName = document.getElementById('inputName');
-  let inputAge = document.getElementById('inputAge');
-  let inputHairColor = document.getElementById('inputHairColor');
-  let inputRelation = document.getElementById('inputRelation');
-  let inputImageUrl = document.getElementById('inputImageUrl');
+    let inputName = document.getElementById("inputName");
+    let inputAge = document.getElementById("inputAge");
+    let inputHairColor = document.getElementById("inputHairColor");
+    let inputRelation = document.getElementById("inputRelation");
+    let inputImageUrl = document.getElementById("inputImageUrl");
 
-  let newPerson = {
-    name: inputName.value,
-    age: inputAge.value,
-    hairColor: inputHairColor.value,
-    relation: inputRelation.value,
-    img: inputImageUrl.value
-  };
-  console.log(newPerson);
+    let newPerson = {
+        name: inputName.value,
+        age: inputAge.value,
+        hairColor: inputHairColor.value,
+        relation: inputRelation.value,
+        img: inputImageUrl.value
+    };
+    console.log(newPerson);
 
-  _familyMembers.push(newPerson);
-  appendPersons(_familyMembers);
+    _familyMembers.push(newPerson);
+    appendPersons(_familyMembers);
 }
