25366a42d718dc107877ae6fec2007888916feaf
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-02-06 16:02:13 +0100

    Update main.js

--- spa-user-crud-jsonbin/js/main.js
+++ spa-user-crud-jsonbin/js/main.js
@@ -3,8 +3,8 @@ let _users = [];
 let _selectedUserId;
 const _baseUrl = "https://api.jsonbin.io/v3/b/61138ef2d5667e403a3fb6a1";
 const _headers = {
-  "X-Master-Key": "$2b$10$Uf1lbMtIPrrWeneN3Wz6JuDcyBuOz.1LbHiUg32QexCCJz3nOpoS2",
-  "Content-Type": "application/json"
+    "X-Master-Key": "$2b$10$Uf1lbMtIPrrWeneN3Wz6JuDcyBuOz.1LbHiUg32QexCCJz3nOpoS2",
+    "Content-Type": "application/json"
 };
 
 // ========== READ ==========
@@ -13,25 +13,25 @@ const _headers = {
  * Fetchs person data from jsonbin
  */
 async function loadUsers() {
-  const url = _baseUrl + "/latest"; // make sure to get the latest version
-  const response = await fetch(url, {
-    headers: _headers
-  });
-  const data = await response.json();
-  console.log(data);
-  _users = data.record;
-  appendUsers(_users);
+    const url = _baseUrl + "/latest"; // make sure to get the latest version
+    const response = await fetch(url, {
+        headers: _headers
+    });
+    const data = await response.json();
+    console.log(data);
+    _users = data.record;
+    appendUsers(_users);
 }
 loadUsers();
 
 /**
  * Appends users to the DOM
- * @param {Array} users 
+ * @param {Array} users
  */
 function appendUsers(users) {
-  let htmlTemplate = "";
-  for (const user of users) {
-    htmlTemplate += /*html*/ `
+    let htmlTemplate = "";
+    for (const user of users) {
+        htmlTemplate += /*html*/ `
       <article>
         <h3>${user.name}</h3>
         <p><a href="mailto:${user.mail}">${user.mail}</a></p>
@@ -39,9 +39,9 @@ function appendUsers(users) {
         <button onclick="deleteUser(${user.id})">Delete</button>
       </article>
       `;
-  }
-  document.querySelector("#grid-users").innerHTML = htmlTemplate;
-  showLoader(false);
+    }
+    document.querySelector("#grid-users").innerHTML = htmlTemplate;
+    showLoader(false);
 }
 
 // ========== CREATE ==========
@@ -50,110 +50,110 @@ function appendUsers(users) {
  * Creates a new user with properties: name, mail & id
  */
 async function createUser() {
-  showLoader(true);
-  // references to input fields
-  let nameInput = document.querySelector("#name");
-  let mailInput = document.querySelector("#mail");
-  // dummy generated user id
-  const userId = Date.now();
-  // declaring a new user object
-  const newUser = {
-    name: nameInput.value,
-    mail: mailInput.value,
-    id: userId
-  };
-  // pushing the new user object to the _users array
-  _users.push(newUser);
-  // wait for update
-  await updateJSONBIN(_users);
-  // reset
-  nameInput.value = "";
-  mailInput.value = "";
-  //navigating back
-  navigateTo("#/");
+    showLoader(true);
+    // references to input fields
+    let nameInput = document.querySelector("#name");
+    let mailInput = document.querySelector("#mail");
+    // dummy generated user id
+    const userId = Date.now();
+    // declaring a new user object
+    const newUser = {
+        name: nameInput.value,
+        mail: mailInput.value,
+        id: userId
+    };
+    // pushing the new user object to the _users array
+    _users.push(newUser);
+    // wait for update
+    await updateJSONBIN(_users);
+    // reset
+    nameInput.value = "";
+    mailInput.value = "";
+    //navigating back
+    navigateTo("#/");
 }
 
 // ========== UPDATE ==========
 
 /**
- * Finds a display selected user by given. 
- * @param id 
+ * Finds a display selected user by given.
+ * @param id
  */
 function selectUser(id) {
-  _selectedUserId = id;
-  // find user by given user id
-  const user = _users.find(user => user.id == _selectedUserId);
-  // references to the input fields
-  let nameInput = document.querySelector('#name-update');
-  let mailInput = document.querySelector('#mail-update');
-  // set indout values with selected user values 
-  nameInput.value = user.name;
-  mailInput.value = user.mail;
-  navigateTo("#/update");
+    _selectedUserId = id;
+    // find user by given user id
+    const user = _users.find(user => user.id == _selectedUserId);
+    // references to the input fields
+    let nameInput = document.querySelector("#name-update");
+    let mailInput = document.querySelector("#mail-update");
+    // set indout values with selected user values
+    nameInput.value = user.name;
+    mailInput.value = user.mail;
+    navigateTo("#/update");
 }
 
 /**
  * Updates user with values from input fields
  */
 async function updateUser() {
-  showLoader(true);
-  // references to input fields
-  const nameInput = document.querySelector("#name-update");
-  const mailInput = document.querySelector("#mail-update");
-  // find user to update by given user id
-  const userToUpdate = _users.find(user => user.id === _selectedUserId);
-  // update values of user in array
-  userToUpdate.name = nameInput.value;
-  userToUpdate.mail = mailInput.value;
-  // wait for update
-  await updateJSONBIN(_users);
-  // reset
-  nameInput.value = "";
-  mailInput.value = "";
-  //navigating back
-  navigateTo("#/");
+    showLoader(true);
+    // references to input fields
+    const nameInput = document.querySelector("#name-update");
+    const mailInput = document.querySelector("#mail-update");
+    // find user to update by given user id
+    const userToUpdate = _users.find(user => user.id === _selectedUserId);
+    // update values of user in array
+    userToUpdate.name = nameInput.value;
+    userToUpdate.mail = mailInput.value;
+    // wait for update
+    await updateJSONBIN(_users);
+    // reset
+    nameInput.value = "";
+    mailInput.value = "";
+    //navigating back
+    navigateTo("#/");
 }
 
 // ========== DELETE ==========
 /**
  * Deletes user by given user id
- * @param id 
+ * @param id
  */
 async function deleteUser(id) {
-  showLoader(true);
-  _users = _users.filter(user => user.id !== id);
-  await updateJSONBIN(_users);
+    showLoader(true);
+    _users = _users.filter(user => user.id !== id);
+    await updateJSONBIN(_users);
 }
 
 // ========== Services ==========
 /**
  * Updates the data source on jsonbin with a given users arrays
- * @param {Array} users 
+ * @param {Array} users
  */
 async function updateJSONBIN(users) {
-  // put users array to jsonbin
-  const response = await fetch(_baseUrl, {
-    method: "PUT",
-    headers: _headers,
-    body: JSON.stringify(users)
-  });
-  // waiting for the result
-  const result = await response.json(); // the new updated users array from jsonbin
-  console.log(result);
-  //updating the DOM with the new fetched users
-  appendUsers(result.record);
+    // put users array to jsonbin
+    const response = await fetch(_baseUrl, {
+        method: "PUT",
+        headers: _headers,
+        body: JSON.stringify(users)
+    });
+    // waiting for the result
+    const result = await response.json(); // the new updated users array from jsonbin
+    console.log(result);
+    //updating the DOM with the new fetched users
+    appendUsers(result.record);
 }
 
 // ========== Loader ==========
 /**
  * Shows or hides loader by giden parameter: true/false
- * @param {boolean} show 
+ * @param {boolean} show
  */
 function showLoader(show) {
-  const loader = document.querySelector('#loader');
-  if (show) {
-    loader.classList.remove("hide");
-  } else {
-    loader.classList.add("hide");
-  }
+    const loader = document.querySelector("#loader");
+    if (show) {
+        loader.classList.remove("hide");
+    } else {
+        loader.classList.add("hide");
+    }
 }
