019fef325a9dfa131af6aba0fd45fdb9199a7542
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-08-26 07:40:36 +0200

    renaming, vars

--- firebase-crud-rest/app.js
+++ firebase-crud-rest/app.js
@@ -1,8 +1,8 @@
-const baseUrl = "https://race-crud-rest-default-rtdb.firebaseio.com";
+const endpoint = "https://race-crud-rest-default-rtdb.firebaseio.com";
 
 // === READ (GET) === //
 async function getUsers() {
-    const res = await fetch(baseUrl + "/users.json");
+    const res = await fetch(endpoint + "/users.json");
     const data = await res.json();
     const userList = Object.keys(data).map(key => ({ id: key, ...data[key] })); // from object to array
     return userList;
@@ -12,7 +12,7 @@ async function getUsers() {
 async function createUser(name, mail, image) {
     const newUser = { name, mail, image };
     const userAsJson = JSON.stringify(newUser);
-    const res = await fetch(baseUrl + "/users.json", { method: "POST", body: userAsJson });
+    const res = await fetch(endpoint + "/users.json", { method: "POST", body: userAsJson });
     console.log(res);
 }
 
@@ -20,13 +20,13 @@ async function createUser(name, mail, image) {
 async function updateUser(id, name, mail, image) {
     const userToUpdate = { name, mail, image };
     const userAsJson = JSON.stringify(userToUpdate);
-    const res = await fetch(baseUrl + `/users/${id}.json`, { method: "PUT", body: userAsJson });
+    const res = await fetch(endpoint + `/users/${id}.json`, { method: "PUT", body: userAsJson });
     console.log(res);
 }
 
 // === DELETE (DELETE) === //
 async function deleteUser(id) {
-    const res = await fetch(baseUrl + `/users/${id}.json`, { method: "DELETE" });
+    const res = await fetch(endpoint + `/users/${id}.json`, { method: "DELETE" });
     console.log(res);
 }
 
