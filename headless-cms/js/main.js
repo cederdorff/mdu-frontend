eada5d58f035c52858b7dd61b7ee509117be7e3a
Rasmus Cederdorff <rasmus@cederdorff.com>
2022-09-21 17:33:07 +0200

    Update main.js

--- headless-cms/js/main.js
+++ headless-cms/js/main.js
@@ -1,38 +1,35 @@
 "use strict";
 
-// const url = "https://api.cederdorff.com/wp-json/wp/v2/posts";
-const url = "https://api.cederdorff.com/wp-json/wp/v2/posts?_embed";
+async function getData() {
+    const url = "https://api.cederdorff.com/wp-json/wp/v2/posts?_embed";
+    const res = await fetch(url);
+    const data = await res.json();
+    appendPosts(data);
+}
 
-fetch(url)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (posts) {
-    console.log(posts);
-    appendPosts(posts);
-  });
+getData();
 
 // append wp posts to the DOM
 function appendPosts(posts) {
-  let htmlTemplate = "";
-  for (let post of posts) {
-    console.log(post);
-    htmlTemplate += /*html*/`
+    let htmlTemplate = "";
+    for (let post of posts) {
+        console.log(post);
+        htmlTemplate += /*html*/ `
       <article>
       <img src="${getFeaturedImageUrl(post)}">
         <h3>${post.title.rendered}</h3>
         <p>${post.content.rendered}</p>
       </article>
     `;
-  }
-  document.querySelector('#content').innerHTML = htmlTemplate;
+    }
+    document.querySelector("#content").innerHTML = htmlTemplate;
 }
 
 // get the featured image url
 function getFeaturedImageUrl(post) {
-  let imageUrl = "";
-  if (post._embedded['wp:featuredmedia']) {
-    imageUrl = post._embedded['wp:featuredmedia'][0].source_url;
-  }
-  return imageUrl;
+    let imageUrl = "";
+    if (post._embedded["wp:featuredmedia"]) {
+        imageUrl = post._embedded["wp:featuredmedia"][0].source_url;
+    }
+    return imageUrl;
 }
